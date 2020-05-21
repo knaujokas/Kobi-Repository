4dd71577e8587fa7fadb3f954eb7b9663758b241
knaujokas <60364176+knaujokas@users.noreply.github.com>
2020-05-21 15:54:22 -0500

    Big changes

--- circles/src/script copy.js
+++ circles/src/script copy.js
@@ -9,11 +9,11 @@ function setup() {
     // retrieve all the SVG paths in the html
     var pathEls = document.querySelectorAll('path');
 
-    for (var i = 0; i < pathEls.length; i++) {
+  for (var i = 0; i < pathEls.length; i++) {   // <--------for selecting all paths//
 
-        // for each path
-        var pathEl = pathEls[i];
-
+        //for each path
+        var pathEl = pathEls[i]; // <---------- i is for all paths, use a number to
+                                                //select a specific path//
         // Set an offset along the path.
         var offset = anime.setDashoffset(pathEl);
 
@@ -22,26 +22,17 @@ function setup() {
         // Animation parameters
         let tmp = anime({
             targets: pathEl,
-            strokeDashoffset: [offset, 0],
-            //duration: anime.random(1000, 3000),
-            //delay: anime.random(0, 2000),
+            strokeDashoffset: [offset, 3],
+            duration: anime.random (3000, 5000),
+            delay: anime.random(250, 1250),
             loop: true,
-            easing: 'easeInOutSine',
-            autoplay: false
+            direction: 'normal',
+            easing: 'linear',
         });
 
-        //tmp.play()
+        tmp.play()
             // storage of animated pats in a collection
         animatedPaths.push(tmp);
     }
-
-}
-
-function mousePressed() {
-    anime({
-        targets: document.querySelectorAll('image'),
-        translateY: -100,
-        scale: 0.25
-    })
 
 }
--- kaneCSS/src.js
+++ kaneCSS/src.js
@@ -1,2 +1,73 @@
-// By class
-anime; 
+// By CSS class
+anime({
+    targets: '.kane.html',
+    translateX: 390; 
+});
+
+//  By Element ID
+let anyName = document.getElementById(".row1")
+anime({
+    targets: IMG_2337.jpeg,
+    translateX: 290,
+    easing: 'easeInOutQuad'
+});
+
+// by element Tag
+let drawings = document.querySelectorAll("svg")
+anime({
+    targets: drawings,
+    translateX: 290,
+    easing: 'easeInQuad'
+});
+
+// By query selector
+let rows = document.querySelectorAll(".txt2")
+anime({
+    targets: rows,
+    translateX: 290,
+    easing: 'easeOutQuad'
+});
+
+// By query selector
+let myRows = document.querySelectorAll(".div")
+anime({
+    targets: myRows,
+    translateX: 290,
+    easing: 'easeOutQuad'
+});
+
+
+// TRANSFORMATIONS
+let transform = document.querySelectorAll(".div")
+anime({
+    targets: transform,
+    translateX: 490,
+    skew: 40,
+    scale: 1.2,
+    easing: 'easeInOutQuad'
+});
+
+// PROPERTIES
+let props = document.querySelectorAll(".div")
+anime({
+    targets: image.2337,
+    animate:
+    translateX: {
+        value: 400,
+        duration: 800
+    },
+    rotate: {
+        value: 330,
+        duration: 1800,
+        easing: 'easeInOutSine'
+    },
+    scale: {
+        value: 6,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    loop: true,
+    direction: 'alternate',
+    delay: 250 // All properties except 'scale' inherit 250ms delay
+});
